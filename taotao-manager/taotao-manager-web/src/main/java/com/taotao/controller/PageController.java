ef48dba6e2978d2f34fb8cefb7db5daf864c28fe
yvettee36 <842207739@qq.com>
2018-07-27 18:00:36 +0800

    test

---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,21 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by yvettee on 2018/7/25.
+ */
+@Controller
+public class PageController {
+    @RequestMapping("/")
+    public String showIndex() {
+        return "index";
+    }
+
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page) {
+        return page;
+    }
+}
